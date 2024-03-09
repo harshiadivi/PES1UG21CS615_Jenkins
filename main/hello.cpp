6c10acf6ff6316d0356cfe132c8999ab816eb841
harshiadivi <adivi.sh2003@gmail.com>
2024-03-09 13:52:57 +0530

    my second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "MY NAME IS HARSHITHA AND MY SRN IS PES1UG21CS615";
   return 0;
 }
